dcaf99a1b28f47d1a2c2b8f99956c2442763b1ed
Zackary Lowery <zlowery@xcjs.com>
2024-05-22 14:18:50 -0400

    Add validation to EnvironmentSettings.

--- models/EnvironmentSettings.js
+++ models/EnvironmentSettings.js
@@ -15,6 +15,11 @@ export class EnvironmentSettings {
 
     #logger = null;
 
+    #validNodeEnvironments = [
+        'development',
+        'production'
+    ];
+
     get isProduction() {
         return this.nodeEnvironment === 'production';
     }
@@ -34,6 +39,7 @@ export class EnvironmentSettings {
 
         this.#logger = new Logger(this.isProduction, 'EnvironmentSettings');
         this.#logConfiguration();
+        this.#validate();
     }
 
     #logConfiguration() {
@@ -43,5 +49,27 @@ export class EnvironmentSettings {
         this.#logger(LogLevel.Info, `EASY_DIFFUSION_DISCORD_BOT_EASY_DIFFUSION_HOSTS: ${this.easyDiffusionHosts.join(', ')}`);
         this.#logger(LogLevel.Info, `EASY_DIFFUSION_DISCORD_BOT_EASY_DIFFUSION_MODEL: ${this.easyDiffusionModel}`);
         this.#logger(LogLevel.Info, `EASY_DIFFUSION_DISCORD_BOT_REQUIRES_MENTION: ${this.botRequiresMention}`);
+    }
+
+    #validate() {
+        if(!this.#validNodeEnvironments.includes(this.nodeEnvironment)) {
+            throw new Error(`NODE_ENV should be one of ${this.#validNodeEnvironments.join(', ')} but is ${this.nodeEnvironment} instead.`);
+        }
+
+        if(this.discordToken === null || this.discordToken.length === 0) {
+            throw new Error(`EASY_DIFFUSION_DISCORD_BOT_TOKEN requires a value.`);
+        }
+
+        if(this.discordChannels.length === 0) {
+            throw new Error(`EASY_DIFFUSION_DISCORD_BOT_CHANNELS requires at least one channel ID.`);
+        }
+
+        if(this.easyDiffusionHosts.length === 0) {
+            throw new Error(`EASY_DIFFUSION_DISCORD_BOT_EASY_DIFFUSION_HOSTS requires at least one value.`);
+        }
+
+        if(this.easyDiffusionModel === null || this.easyDiffusionModel.length === 0) {
+            throw new Error(`EASY_DIFFUSION_DISCORD_BOT_EASY_DIFFUSION_MODEL requires a value.`);
+        }
     }
 }
