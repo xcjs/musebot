2ed46259f969bd7127fb4501afb97818eb9dc740
Zackary Lowery <zlowery@xcjs.com>
2024-07-09 17:11:23 -0400

    Correct several issues in binary build.

--- src/bot.ts
+++ src/bot.ts
@@ -2,7 +2,7 @@ import { EnvironmentSettings } from './models/EnvironmentSettings.js';
 import { DiscordEasyDiffusionClient } from './services/clients/discord/DiscordEasyDiffusionClient.js';
 import { TypingService } from './services/clients/discord/services/TypingService.js';
 
-const environmentSettings = new EnvironmentSettings(false);
+const environmentSettings = new EnvironmentSettings();
 const typingService = new TypingService(environmentSettings);
 
 const client = new DiscordEasyDiffusionClient(environmentSettings, typingService);
