e60e583312a5ad3be046090e780a34b6e51ff73c
Zackary Lowery <zlowery@xcjs.com>
2024-07-09 22:16:07 -0400

    Updated bot to create a new instance of either DiscordClient based on the primary bot function.

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,22 @@
+import { BotFunction } from './enums/BotFunction.js';
 import { EnvironmentSettings } from './models/EnvironmentSettings.js';
+import { BaseDiscordClient } from './services/clients/discord/BaseDiscordClient.js';
 import { DiscordEasyDiffusionClient } from './services/clients/discord/DiscordEasyDiffusionClient.js';
+import { DiscordOllamaClient } from './services/clients/discord/DiscordOllamaClient.js';
 import { TypingService } from './services/clients/discord/services/TypingService.js';
 
 const environmentSettings = new EnvironmentSettings();
 const typingService = new TypingService(environmentSettings);
 
-const client = new DiscordEasyDiffusionClient(environmentSettings, typingService);
+let client: BaseDiscordClient;
+
+switch(environmentSettings.botFunction) {
+    case BotFunction.Images:
+        client = new DiscordEasyDiffusionClient(environmentSettings, typingService);
+        break;
+    case BotFunction.Text:
+        client = new DiscordOllamaClient(environmentSettings, typingService);
+        break;
+}
 
 client.login();
--- src/enums/BotFunction.ts
+++ src/enums/BotFunction.ts
@@ -1,4 +1,4 @@
 export enum BotFunction {
-    image = 'image',
-    text = 'text'
+    Images = 'images',
+    Text = 'text'
 }
