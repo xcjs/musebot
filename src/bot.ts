d0b8bf36b22affeb184fb1ec57165b81ef5e0784
Zackary Lowery <zlowery@xcjs.com>
2024-07-09 21:23:05 -0400

    Move DiscordEasyDiffusionService back to Client.

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,10 @@
 import { EnvironmentSettings } from './models/EnvironmentSettings.js';
-import { DiscordEasyDiffusionService } from './services/DiscordEasyDiffusionService.js';
+import { DiscordEasyDiffusionClient } from './services/clients/discord/DiscordEasyDiffusionClient.js';
 import { TypingService } from './services/clients/discord/services/TypingService.js';
 
 const environmentSettings = new EnvironmentSettings();
 const typingService = new TypingService(environmentSettings);
 
-const client = new DiscordEasyDiffusionService(environmentSettings, typingService);
+const client = new DiscordEasyDiffusionClient(environmentSettings, typingService);
 
 client.login();
