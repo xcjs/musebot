43829a12fd189751f3a727cf2efe01eeac620ec1
Zackary Lowery <zlowery@xcjs.com>
2024-07-13 13:15:55 -0400

    Implement FeatureService in the EasyDiffusion and Ollama clients.

--- src/app.ts
+++ src/app.ts
@@ -4,18 +4,20 @@ import { BaseDiscordClient } from './services/clients/discord/BaseDiscordClient.
 import { DiscordEasyDiffusionClient } from './services/clients/discord/DiscordEasyDiffusionClient.js';
 import { DiscordOllamaClient } from './services/clients/discord/DiscordOllamaClient.js';
 import { TypingService } from './services/clients/discord/services/TypingService.js';
+import { FeatureService } from './services/features/FeatureService.js';
 
 const environmentSettings = new EnvironmentSettings();
 const typingService = new TypingService(environmentSettings);
+const featureService = new FeatureService(environmentSettings);
 
 let client: BaseDiscordClient;
 
 switch(environmentSettings.botFunction) {
     case BotFunction.Images:
-        client = new DiscordEasyDiffusionClient(environmentSettings, typingService);
+        client = new DiscordEasyDiffusionClient(environmentSettings, typingService, featureService);
         break;
     case BotFunction.Text:
-        client = new DiscordOllamaClient(environmentSettings, typingService);
+        client = new DiscordOllamaClient(environmentSettings, typingService, featureService);
         break;
 }
 
