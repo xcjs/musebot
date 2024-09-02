5eafd126d7f7a7ad75dba2fc7818ceb971794311
Zackary Lowery <zlowery@xcjs.com>
2024-09-02 00:38:54 -0400

    Correct for a case where model titles can have a file hash at the end of the virtual path.

--- src/services/clients/automatic1111/tasks/AttachRenderTask.ts
+++ src/services/clients/automatic1111/tasks/AttachRenderTask.ts
@@ -53,7 +53,7 @@ export class AttachRenderTask extends BaseTask {
     override async process(): Promise<void> {
         const model = this.#environmentSettings.stableDiffusionModels.length > 0 ?
             getRandomArrayEntry(this.#environmentSettings.stableDiffusionModels) :
-            getRandomArrayEntry(await this.#automatic1111Client.getModels()).title;
+            getRandomArrayEntry(await this.#automatic1111Client.getModels()).title.split(' ')[0];
 
         this.#logger(LogLevel.Info, `Using ${model} as the selected EasyDiffusion model.`);
 
