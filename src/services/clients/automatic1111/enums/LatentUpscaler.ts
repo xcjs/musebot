1c98e51f690b355e4097cd621e2a0c5720c864fc
Zackary Lowery <zlowery@xcjs.com>
2024-09-07 23:39:56 -0400

    Add LatentUpscaler enum.

---
+++ src/services/clients/automatic1111/enums/LatentUpscaler.ts
@@ -0,0 +1,8 @@
+export enum LatentUpscaler {
+    Latent = 'Latent',
+    LatentAntiAliased = 'Latent (antialiased)',
+    LatentBicubic = 'Latent (bicubic)',
+    LatentBicubicAntiAliased = 'Latent (bicubic antialiased)',
+    LatentNearest = 'Latent (nearest)',
+    LatentNearestExact = 'Latent (nearest-exact)'
+}
