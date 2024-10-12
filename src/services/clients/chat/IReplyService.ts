95981e356dd2d4ec12364728545290e81c5a8d82
Zackary Lowery <zlowery@xcjs.com>
2024-10-12 03:11:11 -0400

    Begin common re-implementation of reply-related services.

--- src/services/clients/chat/IReplyService.ts
+++ src/services/clients/chat/IReplyService.ts
@@ -1,3 +1,14 @@
-export interface IReplyService {
+import { Message, ButtonInteraction, AttachmentBuilder } from 'discord.js';
 
+export interface IReplyService {
+    shouldReply(message: Message): boolean;
+
+    reply(
+        interaction: Message | ButtonInteraction,
+        content: string | null,
+        additionalAttachments: Array<AttachmentBuilder>,
+        isEdit: boolean
+    ): Promise<void>;
+
+    replyWithError(interaction: Message | ButtonInteraction): Promise<void>;
 }
