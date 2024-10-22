1a55f1e298ce5688fff65c78166a9faba30826c8
Zackary Lowery <zlowery@xcjs.com>
2024-10-22 00:04:18 -0400

    Add import sorting rules.

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,22 @@
-import pluginJs from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import pluginJs from '@eslint/js';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"]
-  },
-  {
-    languageOptions: {
-      globals: globals.node
-    }
-  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      globals: globals.node
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  }
 ];
