63c344e0bc16e8024f1890b741b0be3eacfdd77c
Zackary Lowery <zlowery@xcjs.com>
2024-12-13 01:48:17 -0500

    Allow LLM bots to reply to replies.

--- src/services/clients/chat/discord/replies/ReplyService.ts
+++ src/services/clients/chat/discord/replies/ReplyService.ts
@@ -1,6 +1,7 @@
 import { Attachment, AttachmentBuilder, ButtonInteraction, Client as DiscordClient, Message, MessageType, User  } from 'discord.js';
 import { Logger, LogLevel } from 'meklog';
 
+import { BotFunction } from '../../../../../enums/BotFunction.js';
 import { ContentType } from '../../../../../enums/ContentType.js';
 import { getRandomInt } from '../../../../../utilities/random-utilities.js';
 import { splitText } from '../../../../../utilities/string-utilities.js';
@@ -35,8 +36,11 @@ export class ReplyService implements IReplyService {
             return false;
         }
 
-        // The message is not a default message type and not a reaction reply.
-        if (message.type !== MessageType.Default && !isReaction) {
+        // The message is not a default message type and not a reaction reply
+        // unless it's a reply to an LLM.
+        if (message.type !== MessageType.Default
+            && (this.#environmentSettings.botFunction !== BotFunction.Text && message.type !== MessageType.Reply)
+            && !isReaction) {
             this.#logger(LogLevel.Info, 'Not replying to a non-default or non-reaction message.');
             return false;
         }
