ae3da1987edab18d1099e82287eb4b7f48e27384
Zackary Lowery <zlowery@xcjs.com>
2024-12-26 01:38:31 -0500

    Port client login to a task queue task.

---
+++ src/services/clients/chat/discord/tasks/LoginTask.ts
@@ -0,0 +1,46 @@
+import { Client as DiscordClient } from 'discord.js';
+import { Logger, LogLevel } from 'meklog';
+
+import { IEnvironmentSettings } from '../../../../IEnvironmentSettings.js';
+import { IServiceContainer } from '../../../../IServiceContainer.js';
+import { TaskStatus } from '../../../../tasks/enums/TaskStatus.js';
+import { ITaskQueue } from '../../../../tasks/ITaskQueue.js';
+import { BaseTask } from '../../../../tasks/models/BaseTask.js';
+
+export class LoginTask extends BaseTask {
+    get taskChannel(): string {
+        return 'Discord';
+    }
+
+    #services: IServiceContainer;
+
+    #environmentSettings: IEnvironmentSettings;
+    #discordClient: DiscordClient;
+    #taskQueue: ITaskQueue;
+
+    protected logger: Logger;
+
+    constructor(services: IServiceContainer) {
+        super(services);
+
+        this.#services = services;
+
+        this.#environmentSettings = services.environmentSettings;
+        this.#discordClient = services.discordClient;
+        this.#taskQueue = services.taskQueue;
+
+        this.logger = new Logger(this.#environmentSettings.isProduction, LoginTask.name);
+    }
+
+    async process(): Promise<void> {
+        this.logger(LogLevel.Info, 'Attempting Discord login...');
+        await this.#discordClient.login(this.#environmentSettings.discordToken);
+    }
+
+    async postProcess(): Promise<void> {
+        if(this.taskStatus === TaskStatus.Dead) {
+            this.logger(LogLevel.Warning, 'Exhausted the maximum attempts for the login task. Adding a new login task to the queue as the application cannot continue otherwise.');
+            await this.#taskQueue.add(new LoginTask(this.#services));
+        }
+    }
+}
