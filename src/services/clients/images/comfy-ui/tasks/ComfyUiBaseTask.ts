664ed1e45b90acae28b2ffb290f41590782d49b6
Zackary Lowery <zlowery@xcjs.com>
2025-01-01 21:40:21 -0500

    Remove BaseTask.

--- src/services/clients/images/comfy-ui/tasks/ComfyUiBaseTask.ts
+++
@@ -1,8 +0,0 @@
-import { IServiceContainer } from '../../../../IServiceContainer.js';
-import { IWorkflow } from '../models/IWorkflow.js';
-
-export class ComfyUiBaseTask {
-    constructor(services: IServiceContainer) {
-
-    }
-}
