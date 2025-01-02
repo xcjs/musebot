5dfbf9df53f8f8e896981c6be7066195e8c6e0a5
Zackary Lowery <zlowery@xcjs.com>
2025-01-02 01:19:21 -0500

    Complete RandomRenderTask.

--- src/services/clients/images/comfy-ui/tasks/ComfyUiRandomRenderTask.ts
+++ src/services/clients/images/comfy-ui/tasks/ComfyUiRandomRenderTask.ts
@@ -9,19 +9,21 @@ import { IEnvironmentSettings } from '../../../../IEnvironmentSettings.js';
 import { IServiceContainer } from '../../../../IServiceContainer.js';
 import { TaskStatus } from '../../../../tasks/enums/TaskStatus.js';
 import { BaseTask } from '../../../../tasks/models/BaseTask.js';
-import { Automatic1111ReplyService } from '../../../chat/discord/automatic1111/Automatic1111ReplyService.js';
+import { ComfyUiReplyService } from '../../../chat/discord/comfy-ui/ComfyUiReplyService.js';
 import { IReplyService } from '../../../chat/IReplyService.js';
 import { OllamaClient } from '../../../text/ollama/OllamaClient.js';
 import { IRandomRenderTask } from '../../tasks/IRandomRenderTask.js';
-import { Automatic1111Client } from '../Automatic1111Client.js';
-import { Txt2ImgOptionsFactory } from '../factories/Txt2ImgOptionsFactory.js';
+import { ComfyUiClient } from '../ComfyUiClient.js';
+import { WorkflowType } from '../enums/WorkflowType.js';
+import { IWorkflowService } from '../services/IWorkflowService.js';
 
 export class ComfyUiRandomRenderTask extends BaseTask implements IRandomRenderTask {
     #services: IServiceContainer;
 
     #environmentSettings: IEnvironmentSettings;
-    #automatic1111Client: Automatic1111Client;
-    #automatic1111ReplyService: Automatic1111ReplyService;
+    #workflowService: IWorkflowService;
+    #comfyUiClient: ComfyUiClient;
+    #comfyUiReplyService: ComfyUiReplyService;
     #replyService: IReplyService;
 
     #interaction: ButtonInteraction;
@@ -29,7 +31,7 @@ export class ComfyUiRandomRenderTask extends BaseTask implements IRandomRenderTa
     #logger;
 
     override get taskChannel(): string {
-        return `Automatic1111_${this.#automatic1111Client.host}`;
+        return `ComfyUi_${this.#comfyUiClient.host}`;
     }
 
     constructor(services: IServiceContainer, interaction: ButtonInteraction) {
@@ -38,8 +40,9 @@ export class ComfyUiRandomRenderTask extends BaseTask implements IRandomRenderTa
         this.#services = services;
 
         this.#environmentSettings = services.environmentSettings;
-        this.#automatic1111Client = services.automatic1111Client;
-        this.#automatic1111ReplyService = services.automatic1111ReplyService;
+        this.#workflowService = services.workflowService;
+        this.#comfyUiClient = services.comfyUiClient;
+        this.#comfyUiReplyService = services.comfyUiReplyService;
         this.#replyService = services.replyService;
 
         this.#interaction = interaction;
@@ -50,29 +53,42 @@ export class ComfyUiRandomRenderTask extends BaseTask implements IRandomRenderTa
     override async process(): Promise<void> {
         this.#logger(LogLevel.Info, 'Processing a ComfyUiRandomRenderTask...');
 
-        const model = this.#environmentSettings.stableDiffusionModels.length > 0 ?
-            getRandomArrayEntry(this.#environmentSettings.stableDiffusionModels) :
-            getRandomArrayEntry(await this.#automatic1111Client.getModels()).title.split(' ')[0];
+        await this.#workflowService.loadWorkflows();
 
-        this.#logger(LogLevel.Info, `Using ${model} as the selected image generation model.`);
+        const workflows = this.#workflowService.workflows.filter(x =>
+            x.type === WorkflowType.Txt2img
+            || x.type === WorkflowType.Txt2vid);
+
+        const workflow = getRandomArrayEntry(workflows);
+
+        this.#logger(LogLevel.Info, `Using ${workflow.name} as the selected workflow.`);
 
         const ollamaClient = new OllamaClient(this.#services);
-        const prompt = getRandomArrayEntry(this.#environmentSettings.stableDiffusionOllamaPrompts);
-        const exchange = await ollamaClient.sendMessage(prompt, null);
+        const ollamaPrompt = getRandomArrayEntry(this.#environmentSettings.stableDiffusionOllamaPrompts);
+        const ollamaExchange = await ollamaClient.sendMessage(ollamaPrompt, null);
 
-        const request = Txt2ImgOptionsFactory.getCurrentModelSettings(model, exchange.response.response);
-        const renderData = await this.#automatic1111ReplyService.renderImage(request, model);
+        const renderRequest = this.#workflowService.getWorkflowDefaults(workflow);
+        renderRequest.prompt = ollamaExchange.response.response;
+        renderRequest.refreshSeed();
+
+        const prompt = this.#workflowService.renderWorkflow(workflow, renderRequest);
+        const imagesResponse = await this.#comfyUiClient.render(prompt);
+
+        const exchange = {
+            request: renderRequest,
+            response: imagesResponse
+        };
 
         const content = `Two AIs whisper to each other over the the ancient \`TCP/IP\` protocol. They present ${this.#interaction.member} with this.`;
 
-        const promptBuffer = Buffer.from(wrapText(request.prompt, MAX_TEXT_LINE_LENGTH).trim(),
+        const promptBuffer = Buffer.from(wrapText(renderRequest.prompt, MAX_TEXT_LINE_LENGTH).trim(),
             BufferEncoding.UTF8);
 
         const promptAttachment = new AttachmentBuilder(promptBuffer, {
-            name: `${request.prompt.substring(0, MAX_FILE_NAME_LENGTH)}.md`
+            name: `${renderRequest.prompt.substring(0, MAX_FILE_NAME_LENGTH)}.md`
         });
 
-        await this.#automatic1111ReplyService.reply(this.#interaction, renderData, content, [promptAttachment]);
+        await this.#comfyUiReplyService.reply(this.#interaction, exchange, content, [promptAttachment]);
     }
 
     override async postProcess(): Promise<void> {
