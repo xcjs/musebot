8e4603709db27287ca35df2e890093133b6b9b72
Zackary Lowery <zlowery@xcjs.com>
2025-01-08 15:00:12 -0500

    Remove timeout logic.

--- src/services/clients/images/comfy-ui/tasks/ComfyUiBaseTask.ts
+++ src/services/clients/images/comfy-ui/tasks/ComfyUiBaseTask.ts
@@ -2,7 +2,6 @@ import { Logger, LogLevel } from 'meklog';
 
 import { IEnvironmentSettings } from '../../../../IEnvironmentSettings.js';
 import { IServiceContainer } from '../../../../IServiceContainer.js';
-import { TaskStatus } from '../../../../tasks/enums/TaskStatus.js';
 import { BaseTask } from '../../../../tasks/models/BaseTask.js';
 import { ComfyUiClient } from '../ComfyUiClient.js';
 import { IWorkflowService } from '../services/IWorkflowService.js';
@@ -28,26 +27,12 @@ export abstract class ComfyUiBaseTask extends BaseTask {
 
     override async process(): Promise<void> {
         await super.process();
+
+        this.#logger(LogLevel.Info, 'Loading workflows...');
         await this.#workflowService.loadWorkflows();
     }
 
     override async postProcess(): Promise<void> {
         await super.postProcess();
-
-        if(this.taskStatus === TaskStatus.Successful) {
-            const taskDurationSeconds = this.#dateToUnixSeconds(new Date()) - this.#dateToUnixSeconds(this.startedTime);
-            const currentTimeoutMs = this.#environmentSettings.taskTimeoutMilliseconds;
-            const newTimeoutMs = Math.floor(currentTimeoutMs + (taskDurationSeconds * 2 * 1000) / 2);
-
-            this.#environmentSettings.taskTimeoutMilliseconds = newTimeoutMs;
-
-            this.#logger(LogLevel.Info, `This task timeout has been adjusted to ${newTimeoutMs}ms based on current performance metrics.`);
-        } else if(this.taskStatus === TaskStatus.Failed) {
-            this.comfyUiClient.disconnect();
-        }
-    }
-
-    #dateToUnixSeconds(date: Date) {
-        return parseInt((date.getTime() / 1000).toFixed(0));
     }
 }
