03403e42362c65cd623c17a1703a529adad6ce4d
Zackary Lowery <zlowery@xcjs.com>
2025-01-19 19:31:39 -0500

    Add nodeEnvironment tests.

--- src/services/EnvironmentSettings.test.ts
+++ src/services/EnvironmentSettings.test.ts
@@ -1,13 +1,42 @@
+import process from 'node:process';
+
 import { describe, expect, test } from '@jest/globals';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import nodePackage from '../../package.json' with { type: 'json' };
+import { NodeEnvironment } from '../enums/NodeEnvironment.js';
 import { EnvironmentSettings } from './EnvironmentSettings';
 
 describe('EnvironmentSettings', () => {
     describe('packageName', () => {
         test('it should equal the package name in package.json', () => {
             const environmentSettings = new EnvironmentSettings();
+            expect(environmentSettings.packageName).toBe(nodePackage.name);
+        });
+    });
 
-            expect(environmentSettings.packageName).toBe('musebot');
+    describe('version', () => {
+        test('it should equal the version number in package.json', () => {
+            const environmentSettings = new EnvironmentSettings();
+            expect(environmentSettings.version).toBe(nodePackage.version);
+        });
+    });
+
+    describe('nodeEnvironment', () => {
+        test(`it should return test in the testing environment without mocking`, () => {
+            const environmentSettings = new EnvironmentSettings();
+            expect(environmentSettings.nodeEnvironment).toBe(NodeEnvironment.Test);
+        });
+
+        test.each([
+            NodeEnvironment.Development,
+            NodeEnvironment.Production
+        ])(`it should match the NODE_ENV environment variable`, (environment) => {
+            process.env.NODE_ENV = environment;
+            const environmentSettings = new EnvironmentSettings();
+
+            expect(environmentSettings.nodeEnvironment).toBe(environment);
         });
     });
 });
