7e209f953669b9e25956617f0b82f4bffb338ed2
Zackary Lowery <zlowery@xcjs.com>
2025-01-19 19:46:25 -0500

    Mock MUSEBOT_FUNCTION for configurationless environments.

--- src/services/EnvironmentSettings.test.ts
+++ src/services/EnvironmentSettings.test.ts
@@ -5,12 +5,14 @@ import { describe, expect, test } from '@jest/globals';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import nodePackage from '../../package.json' with { type: 'json' };
+import { BotFunction } from '../enums/BotFunction.js';
 import { NodeEnvironment } from '../enums/NodeEnvironment.js';
 import { EnvironmentSettings } from './EnvironmentSettings';
 
 describe('EnvironmentSettings', () => {
     describe('packageName', () => {
         test('it should equal the package name in package.json', () => {
+            process.env.MUSEBOT_FUNCTION = BotFunction.Images;
             const environmentSettings = new EnvironmentSettings();
             expect(environmentSettings.packageName).toBe(nodePackage.name);
         });
@@ -18,6 +20,7 @@ describe('EnvironmentSettings', () => {
 
     describe('version', () => {
         test('it should equal the version number in package.json', () => {
+            process.env.MUSEBOT_FUNCTION = BotFunction.Images;
             const environmentSettings = new EnvironmentSettings();
             expect(environmentSettings.version).toBe(nodePackage.version);
         });
@@ -25,6 +28,7 @@ describe('EnvironmentSettings', () => {
 
     describe('nodeEnvironment', () => {
         test(`it should return test in the testing environment without mocking`, () => {
+            process.env.MUSEBOT_FUNCTION = BotFunction.Images;
             const environmentSettings = new EnvironmentSettings();
             expect(environmentSettings.nodeEnvironment).toBe(NodeEnvironment.Test);
         });
@@ -34,6 +38,7 @@ describe('EnvironmentSettings', () => {
             NodeEnvironment.Production
         ])(`it should match the NODE_ENV environment variable`, (environment) => {
             process.env.NODE_ENV = environment;
+            process.env.MUSEBOT_FUNCTION = BotFunction.Images;
             const environmentSettings = new EnvironmentSettings();
 
             expect(environmentSettings.nodeEnvironment).toBe(environment);
