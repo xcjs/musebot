0821e6b6630e57d601f3c96d52d091e32b0c1d17
Zackary Lowery <zlowery@xcjs.com>
2025-02-01 18:32:02 -0500

    Add test cases around the isProduction property.

--- src/services/environment-settings/EnvironmentSettings.test.ts
+++ src/services/environment-settings/EnvironmentSettings.test.ts
@@ -50,11 +50,38 @@ describe('EnvironmentSettings', () => {
         test.each([
             NodeEnvironment.Development,
             NodeEnvironment.Production
-        ])(`should match the ${EnvironmentKey.NodeEnvironment} environment variable`, (environment) => {
-            process.env[EnvironmentKey.NodeEnvironment] = environment;
+        ])(`should match the ${EnvironmentKey.NodeEnvironment} environment variable`, (nodeEnvironment) => {
+            process.env[EnvironmentKey.NodeEnvironment] = nodeEnvironment;
             const environmentSettings = new EnvironmentSettings();
 
-            expect(environmentSettings.nodeEnvironment).toBe(environment);
+            expect(environmentSettings.nodeEnvironment).toBe(nodeEnvironment);
+
+            // Reset the environment after this test to prevent scope leak.
+            process.env[EnvironmentKey.NodeEnvironment] = NodeEnvironment.Test;
+        });
+    });
+
+    describe('isProduction()', () => {
+        it('should return true when it is production', () => {
+            process.env[EnvironmentKey.NodeEnvironment] = NodeEnvironment.Production;
+
+            const environmentSettings = new EnvironmentSettings();
+
+            expect(environmentSettings.isProduction).toBe(true);
+
+            // Reset the environment after this test to prevent scope leak.
+            process.env[EnvironmentKey.NodeEnvironment] = NodeEnvironment.Test;
+        });
+
+        test.each([
+            NodeEnvironment.Development,
+            NodeEnvironment.Test
+        ])('should return false when not production', (nodeEnvironment: NodeEnvironment) => {
+            process.env[EnvironmentKey.NodeEnvironment] = nodeEnvironment;
+
+            const environmentSettings = new EnvironmentSettings();
+
+            expect(environmentSettings.isProduction).toBe(false);
 
             // Reset the environment after this test to prevent scope leak.
             process.env[EnvironmentKey.NodeEnvironment] = NodeEnvironment.Test;
