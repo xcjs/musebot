ddd8de564b7ae7856aeb2bd54565242b9fda303b
Zackary Lowery <zlowery@xcjs.com>
2025-05-06 01:40:05 -0400

    Improve linting rules for types and async/await.

--- src/services/tasks/TaskQueue.ts
+++ src/services/tasks/TaskQueue.ts
@@ -30,7 +30,7 @@ export class TaskQueue implements ITaskQueue {
         this.#logger = new Logger(this.#environmentSettings.isProduction, 'TaskQueue');
     }
 
-    async add(task: BaseTask): Promise<void> {
+    add(task: BaseTask): void {
         this.#logger(LogLevel.Info, `Adding task ${task.id} to the ${task.taskChannel} queue.`);
 
         let taskChannel: TaskChannel;
@@ -46,7 +46,7 @@ export class TaskQueue implements ITaskQueue {
             taskChannel.queue.push(task);
         }
 
-        this.#processQueue();
+        void this.#processQueue();
     }
 
     async #processQueue(): Promise<void> {
