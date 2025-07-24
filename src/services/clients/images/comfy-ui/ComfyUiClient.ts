4eac5bd3d43acae5863f0e21ade808873ed2be08
Zackary Lowery <zlowery@xcjs.com>
2025-07-24 19:49:58 -0400

    Allow prompt logging to be handled by logging library.

--- src/services/clients/images/comfy-ui/ComfyUiClient.ts
+++ src/services/clients/images/comfy-ui/ComfyUiClient.ts
@@ -47,7 +47,7 @@ export class ComfyUiClient {
         const imagesResponses: ImagesResponse[] = [];
 
         prompts.forEach((prompt) => {
-            this.#logger.info('Sending workflow to ComfyUI:', JSON.stringify(prompt));
+            this.#logger.info('Sending workflow to ComfyUI:', prompt);
             delete prompt.$musebotDefaults;
             imagesPromises.push(this.#client.getImages(prompt));
         });
