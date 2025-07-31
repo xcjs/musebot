ed1c2a775fef469c2ebb10f8003b16e82a37bb93
Zackary Lowery <zlowery@xcjs.com>
2025-07-30 20:58:23 -0400

    Add ComfyUiRetryAudioTask.

--- src/enums/ContentType.ts
+++ src/enums/ContentType.ts
@@ -2,6 +2,7 @@ export enum ContentType {
     Jpeg = 'image/jpeg',
     Jpg = 'image/jpg',
     Json = 'application/json',
+    Mp3 = 'audio/mp3',
     Png = 'image/png',
     WebP = 'image/webp'
 }
