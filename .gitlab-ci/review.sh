14183876bbbb51aecc902e83c3a6021cf0ee3782
Zackary Lowery <zlowery@xcjs.com>
2025-08-09 15:45:24 -0400

    Correct quote escape.

--- .gitlab-ci/review.sh
+++ .gitlab-ci/review.sh
@@ -31,7 +31,7 @@ for file in "${filesToReview}"; do
     local prompt="Review the following code from '${file}', provide suggestions for improvement, coding best practices, improve readability, and maintainability. Remove any code smells and anti-patterns. Provide code examples for your suggestion. Respond in markdown format. If the file does not have any code or does not need any changes, say 'No changes needed.'\n\n"
     local content=$(cat "${file}")
 
-    ollama run --hidethinking ${reviewModel} "${prompt}" "${content}"
+    ollama run "${reviewModel}" "${prompt}" "${content}"
 
     # Append the suggestions to the code review:
     echo "## ${file}\n\n" >> "${reviewReport}"
