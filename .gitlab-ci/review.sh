1e97f3d1f65faffaa670c58a6cfddd994adcfc3e
Zackary Lowery <zlowery@xcjs.com>
2025-08-13 20:09:35 -0400

    Tweak review prompt and set it to output to CI/CD log in addition to the file.

--- .gitlab-ci/review.sh
+++ .gitlab-ci/review.sh
@@ -34,12 +34,18 @@ echo "" >> "${reviewReport}"
 for file in ${filesToReview}; do
     echo "Reviewing ${file}"
 
-    prompt="Review the following code from '${file}', provide suggestions for improvement, coding best practices, improve readability, and maintainability. Remove any code smells and anti-patterns. Provide code examples for your suggestion. If the file does not have any code or does not need any changes, say 'No changes needed.'"
+    prompt="Review the following code from '${file}', provide suggestions for improvement, coding best practices, improve readability, and maintainability. Remove any code smells and anti-patterns. Provide code examples for your suggestion. Keep feedback as brief as possible. If the file does not have any code or does not need any changes, say 'No changes needed.'"
     content=$(cat "${file}")
 
+    header="## ${file}"
     review=$(ollama run "${reviewModel}" "${prompt}" "${content}")
 
-    echo "## ${file}" >> "${reviewReport}"
+    echo "${header}"
+    echo
+    echo
+    echo "${review}"
+
+    echo "${header}" >> "${reviewReport}"
     echo "" >> "${reviewReport}"
     echo "" >> "${reviewReport}"
 
