23b82f014e96f06bb7dd4d4bfa24864f4ecea66f
Zackary Lowery <zlowery@xcjs.com>
2025-08-15 21:01:17 -0400

    Sort public member alphabetically and standardize naming conventions better.

--- src/services/clients/chat/discord/comfy-ui/ComfyUiReplyService.ts
+++ src/services/clients/chat/discord/comfy-ui/ComfyUiReplyService.ts
@@ -135,7 +135,7 @@ export class ComfyUiReplyService {
         const isStatefulResponse = requests.filter((request) => {
             if (request !== null) {
                 const description = JSON.stringify(request);
-                return description.length <= DiscordConstants.AttachmentDescriptionMaxLength;
+                return description.length <= DiscordConstants.MaxAttachmentDescriptionLength;
             } else {
                 return false;
             }
--- src/services/clients/chat/discord/enums/DiscordConstants.ts
+++ src/services/clients/chat/discord/enums/DiscordConstants.ts
@@ -1,9 +1,9 @@
 export class DiscordConstants {
     public static readonly ContentMaxLength = 2000;
-    public static readonly AttachmentDescriptionMaxLength = 1024;
-    public static readonly ShardCountAuto = 'auto';
-    public static readonly MaxRequestsPerSecond = 50;
-    public static readonly MaxButtonsPerActionRow = 5;
+    public static readonly InviteLink = 'https://discord.gg/qZMzFA8Apd';
+    public static readonly MaxAttachmentDescriptionLength = 1024;
     public static readonly MaxAttachmentsPerMessage = 10;
-    public static readonly InviteLink = 'https://discord.gg/qZMzFA8Apd'
+    public static readonly MaxButtonsPerActionRow = 5;
+    public static readonly MaxRequestsPerSecond = 50;
+    public static readonly ShardCountAuto = 'auto';
 }
