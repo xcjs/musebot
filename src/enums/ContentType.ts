55b34537501b104980d2ef699d8aed86bd0dbd15
Zackary Lowery <zlowery@xcjs.com>
2026-02-02 20:18:58 -0500

    Add content type for MP4 video.

--- src/enums/ContentType.ts
+++ src/enums/ContentType.ts
@@ -5,6 +5,7 @@ export enum ContentType {
     Jpg = `${ContentTypeCategory.Image}/jpg`,
     Json = `${ContentTypeCategory.Application}}/json`,
     Mp3 = `${ContentTypeCategory.Audio}/mpeg`,
+    Mp4 = `${ContentTypeCategory.Video}/mp4`,
     Png = `${ContentTypeCategory.Image}/png`,
     Unknown = `${ContentTypeCategory.Application}/octet-stream`,
     WebP = `${ContentTypeCategory.Image}/webp`
