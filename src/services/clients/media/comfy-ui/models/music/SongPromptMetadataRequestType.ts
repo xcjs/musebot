fad3cc3027572ea27e58ff3bf3dd437832f17254
Zackary Lowery <zlowery@xcjs.com>
2026-02-09 03:39:39 -0500

    Remove tschema.

--- src/services/clients/media/comfy-ui/models/music/SongPromptMetadataRequestType.ts
+++ src/services/clients/media/comfy-ui/models/music/SongPromptMetadataRequestType.ts
@@ -1,40 +1,92 @@
-import * as t from 'tschema';
-
 import { IStructuredRequestData } from '../../../../llm/ollama/models/IStructuredRequestData.js';
-
-const schema = t.object({
-    tags: t.array(t.string({
-        description:
-            'Tags are for terms or phrases that describe the song genre(s), mood, instruments, vocals, vocal gender, pitch, and more. '
-            + 'Anything goes, and use a lot of detail. If the prompt already includes tags, include all of them. '
-            + 'You may add additional tags if you choose to. All tags should relate to musical terminology.',
-    })),
-    lyrics: t.string({
-        description:
-            'Do not provide lyrics if they are already part of the prompt. Verses can be marked up with [intro], [verse], [bridge], [chorus], or [outro] tags before each verse. '
-            + 'Instrument hints song instructions can be included in parentheses, such as (guitar solo).',
-    }),
-    keyScale: t.enum([
-        'C major', 'C# major', 'Db major',
-        'D major', 'D# major', 'Eb major',
-        'E major', 'F major', 'F# major',
-        'G major', 'G# major', 'A# major',
-        'Bb major', 'B major', 'C minor',
-        'C# minor', 'Db minor', 'D minor',
-        'D# minor', 'Eb minor', 'E minor',
-        'F minor', 'F# minor', 'Gb minor',
-        'G minor', 'G# minor', 'Ab minor',
-        'A minor', 'A# minor', 'Bb minor',
-    ]),
-    bpm: t.integer(),
-    timeSignature: t.enum([2, 3, 4, 6]),
-});
 
 export const songPromptMetadataRequestData: IStructuredRequestData = {
     systemPrompt: 'Your job is to assist with building a song based on the prompt you are given. '
         + 'The description may or may not already include a comma separated list of descriptive song tags and/or lyrics. '
         + 'You will write lyrics if they are missing. Songs tags should only relate to musical terminology or concepts.',
-    schema: schema
+    schema: {
+        "type": "object",
+        "properties": {
+            "tags": {
+                "type": "array",
+                "description": "Tags are for terms or phrases that describe the song genre(s), mood, instruments, vocals, vocal gender, pitch, and more. Anything goes, and use a lot of detail. If the prompt already includes tags, include all of them. You may add additional tags if you choose to. All tags should relate to musical terminology.",
+                "items": {
+                    "type": "string"
+                }
+            },
+            "lyrics": {
+                "type": "string",
+                "description": "Song lyrics with verses for the provided prompt. Verses can be marked up with [intro], [verse], [bridge], [chorus], or [outro] tags before each verse. Instrument hints or song instructions can be included in parentheses, such as (guitar solo)."
+            },
+            "keyScale": {
+                "enum": [
+                    "C major",
+                    "C# major",
+                    "Db major",
+                    "D major",
+                    "D# major",
+                    "Eb major",
+                    "E major",
+                    "F major",
+                    "F# major",
+                    "G major",
+                    "G# major",
+                    "A# major",
+                    "Bb major",
+                    "B major",
+                    "C minor",
+                    "C# minor",
+                    "Db minor",
+                    "D minor",
+                    "D# minor",
+                    "Eb minor",
+                    "E minor",
+                    "F minor",
+                    "F# minor",
+                    "Gb minor",
+                    "G minor",
+                    "G# minor",
+                    "Ab minor",
+                    "A minor",
+                    "A# minor",
+                    "Bb minor"
+                ]
+            },
+            "bpm": {
+                "type": "number"
+            },
+            "timeSignature": {
+                "enum": [
+                    2,
+                    3,
+                    4,
+                    6
+                ]
+            }
+        },
+        "required": [
+            "tags",
+            "lyrics",
+            "keyScale",
+            "bpm",
+            "timeSignature"
+        ]
+    }
 }
 
-export type SongPromptMetadataRequestType = t.Infer<typeof schema>;
+export type SongPromptMetadataRequestType = {
+    tags: string[],
+    lyrics: string,
+    keyScale: 'C major' | 'C# major' | 'Db major'
+        | 'D major' | 'D# major' | 'Eb major'
+        | 'E major' | 'F major' | 'F# major'
+        | 'G major' | 'G# major' | 'A# major'
+        | 'Bb major' | 'B major' | 'C minor'
+        | 'C# minor' | 'Db minor' | 'D minor'
+        | 'D# minor' | 'Eb minor' | 'E minor'
+        | 'F minor' | 'F# minor' | 'Gb minor'
+        | 'G minor' | 'G# minor' | 'Ab minor'
+        | 'A minor' | 'A# minor' | 'Bb minor'
+    bpm: number,
+    timeSignature: 2 | 3 | 4 | 6
+};
--- src/services/clients/media/comfy-ui/models/music/SongPromptTypeRequestType.ts
+++ src/services/clients/media/comfy-ui/models/music/SongPromptTypeRequestType.ts
@@ -1,22 +1,35 @@
-import * as t from 'tschema';
-
 import { IStructuredRequestData } from '../../../../llm/ollama/models/IStructuredRequestData.js';
-
-const schema = t.object({
-    songPromptType: t.enum(['instrumental', 'lyrical']),
-    promptHasTags: t.boolean({
-        description:
-            'Set to true if the prompt includes a comma separated list of descriptive tags. Otherwise set to false.',
-    }),
-    promptHasLyrics: t.boolean({
-        description:
-            'Set to true if the prompt has any lyrics. Otherwise set to false.',
-    }),
-});
-
-export type SongPromptTypeRequestType = t.Infer<typeof schema>;
 
 export const songPromptTypeRequestTypeData: IStructuredRequestData = {
     systemPrompt: 'Your job is to determine what kind of song prompt is provided, if the song prompt contains a comma separated list of descriptive tags, and if the prompt contains any lyrics.',
-    schema: schema
+    schema: {
+        "type": "object",
+        "properties": {
+            "songPromptType": {
+                "enum": [
+                    "instrumental",
+                    "lyrical"
+                ]
+            },
+            "promptHasTags": {
+                "type": "boolean",
+                "description": "Set to true if the prompt includes a comma separated list of descriptive tags. Otherwise set to false."
+            },
+            "promptHasLyrics": {
+                "type": "boolean",
+                "description": "Set to true if the prompt has any lyrics. Otherwise set to false."
+            }
+        },
+        "required": [
+            "songPromptType",
+            "promptHasTags",
+            "promptHasLyrics"
+        ]
+    }
+};
+
+export type SongPromptTypeRequestType = {
+    songPromptType: 'instrumental | lyrical',
+    promptHasTags: boolean,
+    promptHasLyrics: boolean
 };
