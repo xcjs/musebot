0f0e0e81b7a34fb0ea19b664fdbac554bf3a4f02
Zackary Lowery <zlowery@xcjs.com>
2026-02-13 20:11:01 -0500

    De-jsonify schema objects.

--- src/services/clients/media/comfy-ui/models/music/SongPromptMetadataRequestType.ts
+++ src/services/clients/media/comfy-ui/models/music/SongPromptMetadataRequestType.ts
@@ -5,58 +5,58 @@ export const songPromptMetadataRequestData: IStructuredRequestData = {
         + 'The description may or may not already include a comma separated list of descriptive song tags and/or lyrics. '
         + 'You will write lyrics if they are missing. Songs tags should only relate to musical terminology or concepts.',
     schema: {
-        "type": "object",
-        "properties": {
-            "tags": {
-                "type": "array",
-                "description": "Tags are for terms or phrases that describe the song genre(s), mood, instruments, vocals, vocal gender, pitch, and more. Anything goes, and use a lot of detail. If the prompt already includes tags, include all of them. You may add additional tags if you choose to. All tags should relate to musical terminology.",
-                "items": {
-                    "type": "string"
+        type: 'object',
+        properties: {
+            tags: {
+                type: 'array',
+                description: 'Tags are for terms or phrases that describe the song genre(s), mood, instruments, vocals, vocal gender, pitch, and more. Anything goes, and use a lot of detail. If the prompt already includes tags, include all of them. You may add additional tags if you choose to. All tags should relate to musical terminology.',
+                items: {
+                    type: 'string'
                 }
             },
-            "lyrics": {
-                "type": "string",
-                "description": "Song lyrics with verses for the provided prompt. Verses can be marked up with [intro], [verse], [bridge], [chorus], or [outro] tags before each verse. Instrument hints or song instructions can be included in parentheses, such as (guitar solo)."
+            lyrics: {
+                type: 'string',
+                description: 'Song lyrics with verses for the provided prompt. Verses can be marked up with [intro], [verse], [bridge], [chorus], or [outro] tags before each verse. Instrument hints or song instructions can be included in parentheses, such as (guitar solo).'
             },
-            "keyScale": {
-                "enum": [
-                    "C major",
-                    "C# major",
-                    "Db major",
-                    "D major",
-                    "D# major",
-                    "Eb major",
-                    "E major",
-                    "F major",
-                    "F# major",
-                    "G major",
-                    "G# major",
-                    "A# major",
-                    "Bb major",
-                    "B major",
-                    "C minor",
-                    "C# minor",
-                    "Db minor",
-                    "D minor",
-                    "D# minor",
-                    "Eb minor",
-                    "E minor",
-                    "F minor",
-                    "F# minor",
-                    "Gb minor",
-                    "G minor",
-                    "G# minor",
-                    "Ab minor",
-                    "A minor",
-                    "A# minor",
-                    "Bb minor"
+            keyScale: {
+                enum: [
+                    'C major',
+                    'C# major',
+                    'Db major',
+                    'D major',
+                    'D# major',
+                    'Eb major',
+                    'E major',
+                    'F major',
+                    'F# major',
+                    'G major',
+                    'G# major',
+                    'A# major',
+                    'Bb major',
+                    'B major',
+                    'C minor',
+                    'C# minor',
+                    'Db minor',
+                    'D minor',
+                    'D# minor',
+                    'Eb minor',
+                    'E minor',
+                    'F minor',
+                    'F# minor',
+                    'Gb minor',
+                    'G minor',
+                    'G# minor',
+                    'Ab minor',
+                    'A minor',
+                    'A# minor',
+                    'Bb minor'
                 ]
             },
-            "bpm": {
-                "type": "number"
+            bpm: {
+                type: 'number'
             },
-            "timeSignature": {
-                "enum": [
+            timeSignature: {
+                enum: [
                     2,
                     3,
                     4,
@@ -64,12 +64,12 @@ export const songPromptMetadataRequestData: IStructuredRequestData = {
                 ]
             }
         },
-        "required": [
-            "tags",
-            "lyrics",
-            "keyScale",
-            "bpm",
-            "timeSignature"
+        required: [
+            'tags',
+            'lyrics',
+            'keyScale',
+            'bpm',
+            'timeSignature'
         ]
     }
 }
--- src/services/clients/media/comfy-ui/models/music/SongPromptTypeRequestType.ts
+++ src/services/clients/media/comfy-ui/models/music/SongPromptTypeRequestType.ts
@@ -3,27 +3,27 @@ import { IStructuredRequestData } from '../../../../llm/ollama/models/IStructure
 export const songPromptTypeRequestTypeData: IStructuredRequestData = {
     systemPrompt: 'Your job is to determine what kind of song prompt is provided, if the song prompt contains a comma separated list of descriptive tags, and if the prompt contains any lyrics.',
     schema: {
-        "type": "object",
-        "properties": {
-            "songPromptType": {
-                "enum": [
-                    "instrumental",
-                    "lyrical"
+        type: 'object',
+        properties: {
+            songPromptType: {
+                enum: [
+                    'instrumental',
+                    'lyrical'
                 ]
             },
-            "promptHasTags": {
-                "type": "boolean",
-                "description": "Set to true if the prompt includes a comma separated list of descriptive tags. Otherwise set to false."
+            promptHasTags: {
+                type: 'boolean',
+                description: 'Set to true if the prompt includes a comma separated list of descriptive tags. Otherwise set to false.'
             },
-            "promptHasLyrics": {
-                "type": "boolean",
-                "description": "Set to true if the prompt has any lyrics. Otherwise set to false."
+            promptHasLyrics: {
+                type: 'boolean',
+                description: 'Set to true if the prompt has any lyrics. Otherwise set to false.'
             }
         },
-        "required": [
-            "songPromptType",
-            "promptHasTags",
-            "promptHasLyrics"
+        required: [
+            'songPromptType',
+            'promptHasTags',
+            'promptHasLyrics'
         ]
     }
 };
