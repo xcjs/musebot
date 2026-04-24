bfae66fc5e0551b1072b8306c20f055e89c9dcad
Zackary Lowery <zlowery@xcjs.com>
2026-04-23 22:49:18 -0400

    Remove unused constructor.

--- src/services/parallelization/ParallelStrategy.ts
+++ src/services/parallelization/ParallelStrategy.ts
@@ -2,10 +2,6 @@ import { IParallelizationStrategy } from './IParallelizationStrategy.js';
 import { ResourceType } from './ResourceType.js';
 
 export class ParallelStrategy implements IParallelizationStrategy {
-    constructor() {
-
-    }
-
     getTaskChannel(resourceType: ResourceType, resourceUrl: URL | null): string {
         const parts: string[] = [];
         parts.push(resourceType);
