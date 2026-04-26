cfef751d7fea50c6a51f51caa24df5edd915fe77
Zackary Lowery <zlowery@xcjs.com>
2026-04-25 22:13:52 -0400

    Configure child tasks to have no task channel post processor.

--- src/services/ServiceContainer.ts
+++ src/services/ServiceContainer.ts
@@ -301,7 +301,9 @@ export class ServiceContainer implements IServiceContainer {
     }
 
     getTaskChannelPostProcessor(channelName: string): ITaskChannelPostProcessor {
-        if(channelName.startsWith(ResourceType.LargeLanguageModel)) {
+        if(channelName.includes('Child')) {
+            return new NoOpTaskChannelPostProcessor();
+        } else if(channelName.startsWith(ResourceType.LargeLanguageModel)) {
             return new OllamaTaskChannelPostProcessor(this);
         } else if(channelName.startsWith(ResourceType.Media)) {
             return new ComfyUiTaskChannelPostProcessor(this);
