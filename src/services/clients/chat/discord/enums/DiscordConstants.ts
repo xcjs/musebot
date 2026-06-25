3c9b299c61c6c00ccb5b33a97920f47af32abfe2
Zackary Lowery <zlowery@xcjs.com>
2026-06-25 01:33:11 -0400

    Rename state file from SerializableRenderRequest.dat to Prompt.dat

--- src/services/clients/chat/discord/enums/DiscordConstants.ts
+++ src/services/clients/chat/discord/enums/DiscordConstants.ts
@@ -4,7 +4,7 @@ export class DiscordConstants {
     public static readonly MaxAttachmentDescriptionLength = 1024;
     public static readonly MaxAttachmentsPerMessage = 10;
     public static readonly MaxMediaAttachmentsPerMessage = 10;
-    public static readonly StateFileName = 'SerializableRenderRequest.dat';
+    public static readonly StateFileName = 'Prompt.dat';
     public static readonly MaxButtonsPerActionRow = 5;
     public static readonly MaxRequestsPerSecond = 50;
     public static readonly ShardCountAuto = 'auto';
